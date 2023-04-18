d7fa6c962973bd6724689de406e3704dcc348fc6
Sean Jesiolowski <sean.jesiolowski@gmail.com>
2023-04-18 14:37:53 -0400

    Implement delete-all feature

--- file.py
+++ file.py
@@ -2,11 +2,15 @@ class FileHandler:
     def __init__(self):
         self.filename = 'tasks.txt'
 
-    def save_note(self, note_text):
+    def write_lines(self, note_text):
         with open(self.filename, 'a') as f:
             f.write(note_text + '\n')
 
-    def read_all_notes(self):
+    def read_lines(self):
         with open(self.filename, 'r') as f:
             file_lines = f.readlines()
         return file_lines
+
+    def delete_lines(self):
+        with open(self.filename, 'w') as f:
+            f.write('')
--- tk_app.py
+++ tk_app.py
@@ -8,30 +8,36 @@ class MainApplication(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
-        # build app from here
         self.my_entry = tk.Entry(self.parent)
         self.my_entry.focus_set()
-        self.my_entry.pack()
+        self.my_entry.pack(padx=10, pady=10)
 
         self.my_button = tk.Button(self.parent, text='Create note', command=self.click_create_note)
-        self.my_button.pack()
+        self.my_button.pack(padx=10, pady=10)
+
+        self.reveal_button = tk.Button(self.parent, text='All notes visible', command=self.all_notes_visible)
+        self.reveal_button.pack(padx=15, pady=15)
+
+        self.delete_button = tk.Button(self.parent, text='Delete all notes', command=self.delete_notes)
+        self.delete_button['bg'] = 'red'
+        self.delete_button.pack(padx=15, pady=15)
 
     def click_create_note(self):
         entry_content = self.my_entry.get()
         note_in_hand = Notes()
         note_in_hand.write_to_note(entry_content)
         f_hand = FileHandler()
-        f_hand.save_note(entry_content)
-        self.render_notes(False)  # False -> is from memory, NOT from file
+        f_hand.write_lines(entry_content)
+        self.render_notes(False)
 
     def render_notes(self, is_from_file):
         if is_from_file:
             handle = FileHandler()
-            for line_text in handle.read_all_notes():
+            for line_text in handle.read_lines():
                 new_note_obj = Notes()
                 new_note_obj.note_content = line_text.strip()
         else:
-            existing_button_widgets = self.parent.winfo_children()[3:]
+            existing_button_widgets = self.parent.winfo_children()[5:]
             for widget in existing_button_widgets:
                 widget.destroy()
         for index, note in enumerate(Notes.notes_list):
@@ -39,9 +45,20 @@ class MainApplication(tk.Frame):
                 note_button = tk.Button(self.parent, name=str(index), command=lambda idx=index: self.hide_note(idx))
                 note_button['text'] = note.note_content
                 note_button['bg'] = 'yellow'
-                note_button.pack()
+                note_button.pack(padx=5, pady=5)
 
     def hide_note(self, button_index):
         note_index = button_index
         Notes.notes_list[note_index].visible = False
+        self.render_notes(False)
+
+    def all_notes_visible(self):
+        for note in Notes.notes_list:
+            note.visible = True
+        self.render_notes(False)
+
+    def delete_notes(self):
+        handle = FileHandler()
+        handle.delete_lines()
+        Notes.notes_list = []
         self.render_notes(False)
